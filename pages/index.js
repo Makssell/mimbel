b8114422bc8be498ade93e0fecbca59b46a11943
Makssell <68467446+Makssell@users.noreply.github.com>
2025-02-06 22:09:48 +0100

    containers

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
 // pages/index.js
 import Link from 'next/link';
+import styles from './index.module.css'; // Import a CSS module for styling
+
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Main Page</h1>
-      <ul>
-        <li>
-          <Link href="/site1">Site 1</Link>
-        </li>
-        <li>
-          <Link href="/site2">Site 2</Link>
-        </li>
-        <li>
-          <Link href="/site3">Site 3</Link>
-        </li>
-      </ul>
+      <div className={styles.linksContainer}>
+        <Link href="/site1">
+          <a className={styles.link}>Site 1</a>
+        </Link>
+        <Link href="/site2">
+          <a className={styles.link}>Site 2</a>
+        </Link>
+        <Link href="/site3">
+          <a className={styles.link}>Site 3</a>
+        </Link>
+      </div>
     </div>
   );
 };
