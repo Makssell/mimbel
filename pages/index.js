f300e4796ee1ba7ae973ec718542d18cc316a2bf
Makssell <68467446+Makssell@users.noreply.github.com>
2025-02-06 21:57:59 +0100

    0.003

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
-import Navbar from '../components/Navbar';
+// pages/index.js
+import Link from 'next/link';
 
-const Home = () => (
-  <div>
-    <Navbar />
-    <h1>Main Page</h1>
-    <ul>
-      <li><Link href="/site1">Site 1</Link></li>
-      <li><Link href="/site2">Site 2</Link></li>
-      <li><Link href="/site3">Site 3</Link></li>
-    </ul>
-  </div>
-);
+const Home = () => {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <ul>
+        <li>
+          <Link href="/site1">Site 1</Link>
+        </li>
+        <li>
+          <Link href="/site2">Site 2</Link>
+        </li>
+        <li>
+          <Link href="/site3">Site 3</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Home;
